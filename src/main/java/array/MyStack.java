96f28c761ae65a4259939320de6cb62ad2a68315
takatsugu ishikawa <tissi0708@gmail.com>
2013-05-25 03:11:30 +0900

    add constructor

--- src/main/java/array/MyStack.java
+++ src/main/java/array/MyStack.java
@@ -8,6 +8,16 @@ public class MyStack {
 
     private static final int DEFAULT_STACK_SIZE = 100;
 
+    public MyStack() {
+        this(DEFAULT_STACK_SIZE);
+    }
+
+    public MyStack(int size) {
+        stack = new Object[size];
+        stackSize = size;
+        sp = 0;
+    }
+
     public static void main(String[] args) {
         System.out.println("start!");
     }
