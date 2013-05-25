397aaa6a6bf3bd0ad2df0370c98c974298a04128
takatsugu ishikawa <tissi0708@gmail.com>
2013-05-25 14:49:13 +0900

    add push()

--- src/main/java/array/MyStack.java
+++ src/main/java/array/MyStack.java
@@ -24,6 +24,13 @@ public class MyStack {
         Arrays.fill(stack, 0, sp, null);
     }
 
+    public void push(Object x) {
+        if (sp >= stackSize) {
+            throw new IllegalStateException("Stack overflow");
+        }
+        stack[sp++] = x;
+    }
+
     public static void main(String[] args) {
         System.out.println("start!");
     }
